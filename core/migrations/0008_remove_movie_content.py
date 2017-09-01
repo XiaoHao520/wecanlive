843cceca2e03710a7af21a14c6dd85459c0c3f23
run <maibairun@163.com>
2017-09-01 15:39:22 +0800

    更新AdminLog的保存

--- core/migrations/0008_remove_movie_content.py
+++ core/migrations/0008_remove_movie_content.py
@@ -12,8 +12,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.RemoveField(
-        #     model_name='movie',
-        #     name='content',
-        # ),
+        migrations.RemoveField(
+            model_name='movie',
+            name='content',
+        ),
     ]
