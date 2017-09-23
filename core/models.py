45b4713493f054a0318a34e6c1da93e49e3c0cfd
pjb1994 <422061418@qq.com>
2017-09-23 16:49:05 +0800

    VIP回赠

--- core/models.py
+++ core/models.py
@@ -639,6 +639,7 @@ class Member(AbstractMember,
         :return:
         """
         self.vip_level = level
+        self.vip_upgrade_award(level)
         if date_update_vip:
             self.date_update_vip = date_update_vip
             self.make_update_vip_plan(date_update_vip + timedelta(days=30 * level), self.user_id)
@@ -649,6 +650,41 @@ class Member(AbstractMember,
         else:
             self.amount_extend = 0
         self.save()
+
+    def vip_upgrade_award(self, level):
+        """
+        vip 升级时回馈礼物
+        """
+        vip_rebate = Option.get(key='vip_rebate')
+        if not vip_rebate:
+            return
+        vip_award = json.loads(vip_rebate)[level - 1]
+        if vip_award['diamond']:
+            # 钻石
+            CreditDiamondTransaction.objects.create(
+                user_debit=self.user,
+                amount=vip_award['diamond'],
+                type=CreditDiamondTransaction.TYPE_VIP,
+                remark='升級VIP獎勵',
+            )
+        if vip_award['coin']:
+            # 金币
+            CreditCoinTransaction.objects.create(
+                user_debit=self.user,
+                amount=vip_award['coin'],
+                type=CreditCoinTransaction.TYPE_VIP,
+                remark='升級VIP獎勵',
+            )
+        if vip_award['prize']:
+            # 礼物
+            for prize in vip_award['prize']:
+                PrizeTransaction.objects.create(
+                    user_debit=self.user,
+                    amount=prize['amount'],
+                    prize=Prize.objects.get(pk=prize['id']),
+                    type=PrizeTransaction.TYPE_VIP_GAIN,
+                    source_tag=PrizeTransaction.SOURCE_TAG_VIP,
+                )
 
     @staticmethod
     def make_update_vip_plan(date_planned, member_id):
@@ -807,7 +843,7 @@ class Member(AbstractMember,
             return member
         check_history = self.check_member_history.split(',')
         if len(check_history) >= 10 and not str(member.user.id) in check_history \
-                and self.get_vip_level() < 5 and self.get_level() < 5:
+                and self.get_vip_level() < 1 and self.large_level < 2:
             # 查看已经超过10个并且没有会员等级
             return False
         if str(member.user.id) in check_history:
@@ -1307,6 +1343,7 @@ class CreditDiamondTransaction(AbstractTransactionModel):
     TYPE_ACTIVITY_EXPENSE = 'ACTIVITY_EXPENSE'
     TYPE_BOX = 'BOX'
     TYPE_ACTIVITY = 'ACTIVITY'
+    TYPE_VIP = 'VIP'
     TYPE_CHOICES = (
         (TYPE_ADMIN, '管理員發放'),
         (TYPE_LIVE_GIFT, '直播赠送'),
@@ -1314,7 +1351,7 @@ class CreditDiamondTransaction(AbstractTransactionModel):
         (TYPE_WITHDRAW, '提現'),
         (TYPE_ACTIVITY_EXPENSE, '活動消費'),
         (TYPE_BOX, '开启元气宝盒'),
-        (TYPE_ACTIVITY, '活动'),
+        (TYPE_VIP, 'VIP赠送'),
     )
 
     type = models.CharField(
@@ -1341,6 +1378,7 @@ class CreditCoinTransaction(AbstractTransactionModel):
     TYPE_ACTIVITY = 'ACTIVITY'
     TYPE_MISSION = 'MISSION'
     TYPE_ENTER_LIVE = 'ENTER_LIVE'
+    TYPE_VIP = 'VIP'
     TYPE_CHOICES = (
         (TYPE_ADMIN, '管理員發放'),
         (TYPE_LIVE_GIFT, '直播赠送'),
@@ -1353,6 +1391,7 @@ class CreditCoinTransaction(AbstractTransactionModel):
         (TYPE_ACTIVITY, '活动'),
         (TYPE_MISSION, '任務'),
         (TYPE_ENTER_LIVE, '收费直播间'),
+        (TYPE_VIP, 'vip赠送'),
     )
 
     type = models.CharField(
@@ -2603,8 +2642,12 @@ class Live(UserOwnedModel,
         return self.comments.count()
 
     def get_view_count(self):
+        # return LiveWatchLog.objects.filter(
+        #     live=self.id
+        # ).count()
         return LiveWatchLog.objects.filter(
-            live=self.id
+            models.Q(live=self.id, date_leave=None) |
+            models.Q(live=self.id, date_leave__lt=models.F('date_enter'))
         ).count()
 
     def get_prize_count(self):
