86a519acd53450fc8da06d4c9ad02512752fca35
Hugh <139589115@qq.com>
2017-10-13 18:49:55 +0800

    update cron

--- core/cron.py
+++ core/cron.py
@@ -28,7 +28,9 @@ class AutomaticShelvesCronJob(CronJobBase):
         if not rank_record_target_plan:
             date_planned = now + timedelta(minutes=15)
             m.PlannedTask.make('update_rank_record', date_planned)
+        print('update_rank_record Finish')
 
+        # 每天更新一次成员信息
         update_member_check_history_plan = m.PlannedTask.objects.filter(
             method='update_member_check_history',
             date_planned__gt=now,
@@ -36,7 +38,9 @@ class AutomaticShelvesCronJob(CronJobBase):
         if not update_member_check_history_plan:
             date_planned = datetime(now.year, now.month, now.day) + timedelta(days=1)
             m.PlannedTask.make('update_member_check_history', date_planned)
+        print('update_member_check_history Finish')
 
+        # 每天更新一次
         update_activity_settle_plan = m.PlannedTask.objects.filter(
             method='settle_activity',
             date_planned__gt=now,
@@ -44,7 +48,9 @@ class AutomaticShelvesCronJob(CronJobBase):
         if not update_activity_settle_plan:
             date_planned = datetime(now.year, now.month, now.day) + timedelta(days=1)
             m.PlannedTask.make('settle_activity', date_planned)
+        print('settle_activity Finish')
 
+        # 每分钟更新一次热门直播
         update_live_hot_ranking = m.PlannedTask.objects.filter(
             method='update_live_hot_ranking',
             date_planned__gt=now,
@@ -52,7 +58,9 @@ class AutomaticShelvesCronJob(CronJobBase):
         if not update_live_hot_ranking:
             date_planned = now + timedelta(minutes=1)
             m.PlannedTask.make('update_live_hot_ranking', date_planned)
+        print('update_live_hot_ranking Finish')
 
+        # 每分钟更新一次直播结束
         update_live_end = m.PlannedTask.objects.filter(
             method='update_live_end',
             date_planned__gt=now,
@@ -60,7 +68,9 @@ class AutomaticShelvesCronJob(CronJobBase):
         if not update_live_end:
             date_planned = now + timedelta(minutes=1)
             m.PlannedTask.make('update_live_end', date_planned)
+        print('update_live_end Finish')
 
+        # 每分钟更新一次直播日志
         update_log_leave = m.PlannedTask.objects.filter(
             method='update_live_log_leave',
             date_planned__gt=now,
@@ -68,3 +78,4 @@ class AutomaticShelvesCronJob(CronJobBase):
         if not update_log_leave:
             date_planned = now + timedelta(minutes=1)
             m.PlannedTask.make('update_live_log_leave', date_planned)
+        print('update_live_log_leave Finish')
